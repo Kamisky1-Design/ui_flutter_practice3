aa1a50c64683f5d422b5a2478ddb5b8e1899886e
Kamisky <kamilousanussi@gmail.com>
2026-06-30 00:46:07 +0000

    Added CustomSearchBar

---
+++ lib/search_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  const CustomSearchBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 44,
+      child: Row(
+        children: [Container(
+            height: 44,
+            width: 327,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(15),
+            ),
+          
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                 crossAxisAlignment: CrossAxisAlignment.start, 
+                children: [
+                  Icon( 
+                    Icons.search, 
+                    color: Colors.black38,
+                    size: 35,),
+                    Center(
+                      child: Text('Search..', 
+                      style: TextStyle(
+                        color: Colors.black38,
+                        fontSize: 18,),
+                      )
+                      ),
+                ],
+              
+              ),
+            ),
+
+          ),
+          Spacer(),
+          Container(
+            height: 44,
+            width: 44,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Icon(
+              Icons.tune_sharp, 
+              color: Colors.black,),
+          )
+        ],
+      ),
+
+    );
+  }
+}
